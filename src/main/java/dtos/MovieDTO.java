b56828a6b043b1fb8befd01a2b255ef90418f6f8
Christian C <cph-cs450@cphbusiness.dk>
2022-02-23 15:56:49 +0100

    ny

---
+++ src/main/java/dtos/MovieDTO.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtos;
+
+import entities.Movie;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author tha
+ */
+public class MovieDTO {
+    private long id;
+    private String str1;
+    private String str2;
+
+    public MovieDTO(String name, String time) {
+        this.str1 = name;
+        this.str2 = time;
+    }
+    
+    public static List<MovieDTO> getDtos(List<Movie> rms){
+        List<MovieDTO> rmdtos = new ArrayList();
+        rms.forEach(rm->rmdtos.add(new MovieDTO(rm)));
+        return rmdtos;
+    }
+
+
+    public MovieDTO(Movie rm) {
+        if(rm.getId() != null)
+            this.id = rm.getId();
+        this.str1 = rm.getMovieName();
+        this.str2 = rm.getTime();
+    }
+
+    public String getMovieName() {
+        return str1;
+    }
+
+    public void setMovieName(String movieName) {
+        this.str1 = movieName;
+    }
+
+    public String getTime() {
+        return str2 ;
+    }
+
+    public void setTime(String time) {
+        this.str2 = time;
+    }
+
+    @Override
+    public String toString() {
+        return "RenameMeDTO{" + "id=" + id + ", str1=" + str1 + ", str2=" + str2 + '}';
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ src/main/java/entities/Movie.java
@@ -0,0 +1,60 @@
+
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+
+@Entity
+@NamedQuery(name = "Movie.deleteAllRows", query = "DELETE from Movie")
+public class Movie implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String movieName;
+    private String time;
+
+
+    public Movie() {
+    }
+
+    public Movie(String movieName, String time) {
+        this.movieName = movieName;
+        this.time = time;
+    }
+
+    // TODO, delete this class, or rename to an Entity class that makes sense for what you are about to do
+    // Delete EVERYTHING below if you decide to use this class, it's dummy data used for the initial demo
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName1(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+
+}
